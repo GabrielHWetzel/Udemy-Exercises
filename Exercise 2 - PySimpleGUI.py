83855901b3abfd319d62b2e920e6bbd030ec02a1
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-12 09:48:31 -0300

    Added exit and exception to exercise 2

--- Exercise 2 - PySimpleGUI.py
+++ Exercise 2 - PySimpleGUI.py
@@ -10,17 +10,25 @@ inch_input = sg.Input(key="inch")
 convert = sg.Button("Convert")
 meter_text = sg.Text()
 
+exit_button = sg.Button("Exit")
+
+left_column = sg.Column([[feet_label], [inch_label]])
+right_column = sg.Column([[feet_input], [inch_input]])
 window = sg.Window("Convertor",
-                   layout=[[feet_label, feet_input],
-                           [inch_label, inch_input],
-                           [convert, meter_text]])
+                   layout=[[left_column, right_column],
+                           [convert, meter_text, sg.Push(), exit_button]])
 while True:
     event, values = window.read()
     print(event, values)
     match event:
         case "Convert":
-            meter_text.update(f"{fic.convert(float(values['feet']),float(values['inch']))} meters")
+            try:
+                meter_text.update(f"{fic.convert(float(values['feet']),float(values['inch']))} meters")
+            except ValueError:
+                sg.Popup("Please provide two numbers")
         # Quit
+        case "Exit":
+            break
         case sg.WINDOW_CLOSED:
             break
 
