df701460a7d2fedc47c8d62af008d42245be48c6
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-15 08:30:34 -0300

    Exercise 4 Best company page complete

---
+++ Exercise4IncWeb.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas
+
+# Initial Declarations
+with open("Files/loremIpsum.txt", 'r') as lp:
+    loremIpsum = lp.readline()
+
+data = pandas.read_csv("Exercise4/data.csv")
+
+st.set_page_config(
+    layout="wide"
+)
+
+# Page
+st.title("The Best Company")
+st.write(loremIpsum)
+st.subheader("Our Team")
+
+col1, empty1, col2, empty2, col3 = st.columns(5)
+
+with col1:
+    for index, item in data[:4].iterrows():
+        st.subheader(f"{item['first name']} {item['last name']}".title())
+        st.write(item['role'])
+        st.image(f"Exercise4/{item['image']}")
+
+with col2:
+    for index, item in data[4:8].iterrows():
+        st.subheader(f"{item['first name']} {item['last name']}".title())
+        st.write(item['role'])
+        st.image(f"Exercise4/{item['image']}")
+
+with col3:
+    for index, item in data[8:].iterrows():
+        st.subheader(f"{item['first name']} {item['last name']}".title())
+        st.write(item['role'])
+        st.image(f"Exercise4/{item['image']}")
