c0ed0d301043c17c816f2bbd339785eb4f6860b6
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-15 08:35:04 -0300

    No need for empty columns

--- Exercise4IncWeb.py
+++ Exercise4IncWeb.py
@@ -4,9 +4,7 @@ import pandas
 # Initial Declarations
 with open("Files/loremIpsum.txt", 'r') as lp:
     loremIpsum = lp.readline()
-
 data = pandas.read_csv("Exercise4/data.csv")
-
 st.set_page_config(
     layout="wide"
 )
@@ -16,7 +14,7 @@ st.title("The Best Company")
 st.write(loremIpsum)
 st.subheader("Our Team")
 
-col1, empty1, col2, empty2, col3 = st.columns(5)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     for index, item in data[:4].iterrows():
