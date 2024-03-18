7a1afdccd8fcf2c881459eb37e106a4f3fc37166
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-18 10:33:57 -0300

    typo on df["topic"] fixed

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -2,12 +2,12 @@ import streamlit as st
 from Functions.send_email import send_email
 import pandas
 
-df = pandas.read_csv("../Exercise4/topics.csv")
+df = pandas.read_csv("Exercise4/topics.csv")
 
 with st.form(key="form"):
     user_email = st.text_input("Your Email Address")
     topic = st.selectbox("What topic do you want to discuss?",
-                         df["topics"])
+                         df["topic"])
     message = st.text_area("Your message.")
     email_message = f"""Subject: Contact: {topic} - {user_email}
 
