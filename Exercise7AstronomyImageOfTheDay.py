28d76107b7f609bc7bf1b7d4fb12ec8ba1e00a1d
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-21 11:14:00 -0300

    Exercise7 completed

---
+++ Exercise7AstronomyImageOfTheDay.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import requests
+import os
+
+# API key from https://api.nasa.gov
+api_key = os.getenv("NASA_API_KEY")
+
+# Get content
+url = "https://api.nasa.gov/planetary/apod?"\
+    f"api_key={api_key}"
+response = requests.get(url)
+content = response.json()
+
+# Get image
+image_response = requests.get(content["url"])
+image_path = "Images/iotd.png"
+
+# Content
+title = content["title"]
+description = content["explanation"]
+
+with open(image_path, "wb") as file:
+    file.write(image_response.content)
+
+# Web Page
+st.title(title)
+st.image(image_path)
+st.write(description)
+
