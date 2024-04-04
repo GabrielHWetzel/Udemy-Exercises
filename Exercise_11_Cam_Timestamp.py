e8f9fc0855dc2dd4cac1c9e8f74837760bd7d7df
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-04 10:46:25 -0300

    Completed: Exercise 11

--- Exercise_11_Cam_Timestamp.py
+++ Exercise_11_Cam_Timestamp.py
@@ -1,5 +1,6 @@
 import cv2
 import streamlit as st
+import datetime
 
 st.title("Camera")
 start = st.button("Start Camera")
@@ -11,12 +12,13 @@ if start:
     while True:
         check, frame = camera.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        now = datetime.datetime.now()
 
-        cv2.putText(img=frame, text="Temp", org=(10, 30),
+        cv2.putText(img=frame, text=now.strftime('%A'), org=(10, 30),
                     fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
                     color=(255, 255, 255),
                     thickness=1, lineType=cv2.LINE_AA)
-        cv2.putText(img=frame, text="Temp", org=(10, 60),
+        cv2.putText(img=frame, text=now.strftime('%H:%M:%S'), org=(10, 60),
                     fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
                     color=(255, 0, 0),
                     thickness=1, lineType=cv2.LINE_AA)
