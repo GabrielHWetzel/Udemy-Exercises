bdbda2e963494145bcdb85e467b18c1533d6bca8
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-04 10:38:28 -0300

    Started: Exercise 11

---
+++ Exercise_11_Cam_Timestamp.py
@@ -0,0 +1,24 @@
+import cv2
+import streamlit as st
+
+st.title("Camera")
+start = st.button("Start Camera")
+
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        check, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        cv2.putText(img=frame, text="Temp", org=(10, 30),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
+                    color=(255, 255, 255),
+                    thickness=1, lineType=cv2.LINE_AA)
+        cv2.putText(img=frame, text="Temp", org=(10, 60),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2,
+                    color=(255, 0, 0),
+                    thickness=1, lineType=cv2.LINE_AA)
+
+        streamlit_image.image(frame)
