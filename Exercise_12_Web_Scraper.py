430718f401cf0751c287b9853dbd0ba2d9b46cce
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-08 13:35:31 -0300

    Task completed: Exercise 12

--- Exercise_12_Web_Scraper.py
+++ Exercise_12_Web_Scraper.py
@@ -1,4 +1,30 @@
+import requests
+import selectorlib
+from datetime import datetime
 
 URL = "https://programmer100.pythonanywhere.com"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape(url):
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("Exercise12/extractTemp.yaml")
+    value = extractor.extract(source)["temperature"]
+    return value
+
+
+def store(data):
+    now = datetime.now()
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
+    with open("Exercise12/data", 'a') as file:
+        file.write(f"{now},{data}\n")
+
+
+if __name__ == "__main__":
+    store(extract(scrape(URL)))
